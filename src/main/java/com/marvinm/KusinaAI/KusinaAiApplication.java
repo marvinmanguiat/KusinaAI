4c12067768f200398197250f066eb016f0e357c6
marvin manguiat <marvin.manguiat.u@gmail.com>
2026-07-17 09:33:34 +0800

    Initial Spring Boot + React project

---
+++ src/main/java/com/marvinm/KusinaAI/KusinaAiApplication.java
@@ -0,0 +1,13 @@
+package com.marvinm.KusinaAI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KusinaAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KusinaAiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marvinm/KusinaAI/KusinaAiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marvinm.KusinaAI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KusinaAiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
